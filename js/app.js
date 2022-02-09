6ab8c70b0f000ff522ae73b1d515570b6e48df9e
Gridin Egor <egorka.gridin@gmail.com>
2022-02-10 00:24:23 +0300

    create html css js files

---
+++ js/app.js
@@ -0,0 +1,38 @@
+var calculator = document.querySelector('calculator')
+
+colculator.onClick = function(event){
+    alert(event.target.tagName)
+}
+
+var operation = null``
+var saveNumber = null
+
+function calc(value){
+    switch(value){
+        case '=':
+
+        break
+        case '+':
+
+        break
+        case '-':
+
+        break
+        case '*':
+
+        break
+        case '/':
+
+        break
+        case 'c':
+
+        break
+        case 'ce':
+
+        break
+        default:
+
+        break
+    }
+}
+
